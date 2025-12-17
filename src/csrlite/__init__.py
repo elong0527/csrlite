e4cd5f637bddf2ca79939c9f5bf0968a4d29af81
hellomingchun <hellomingchun@gmail.com>
2025-12-17 16:21:17 -0600

    DS Listing

--- src/csrlite/__init__.py
+++ src/csrlite/__init__.py
@@ -31,6 +31,7 @@ from .ie.ie import (
     ie_df,
     ie_rtf,
     study_plan_to_ie_summary,
+    study_plan_to_ie_listing,
 )
 
 # Configure logging
@@ -66,4 +67,5 @@ __all__ = [
     "ie_df",
     "ie_rtf",
     "study_plan_to_ie_summary",
+    "study_plan_to_ie_listing",
 ]
